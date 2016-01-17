15bb528e48b645e5f3995463db8ea11527255fca
koquantam <quyenach1234@gmail.com>
2016-01-17 13:32:59 +0700

    Cleanup and re-organization

--- device_vivalto3gvn.mk
+++ device_vivalto3gvn.mk
@@ -12,9 +12,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-LOCAL_PATH := device/samsung/vivalto3gvn
-
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+# Inherit from fortuna3g device
 $(call inherit-product, $(LOCAL_PATH)/device.mk)
 
 # Keylayouts
@@ -24,26 +23,111 @@ PRODUCT_COPY_FILES += \
 
 # board-specific files
 PRODUCT_COPY_FILES += \
-	device/samsung/vivalto3gvn/audio_params/tiny_hw.xml:system/etc/tiny_hw.xml		\
-	device/samsung/vivalto3gvn/audio_params/codec_pga.xml:system/etc/codec_pga.xml		\
-	device/samsung/vivalto3gvn/audio_params/audio_hw.xml:system/etc/audio_hw.xml		\
-	device/samsung/vivalto3gvn/audio_params/audio_para:system/etc/audio_para		\
-	device/samsung/vivalto3gvn/audio_params/audio_policy.conf:system/etc/audio_policy.conf
+	device/samsung/vivalto3gvn/ramdisk/init.board.rc:root/init.board.rc \
+	device/samsung/vivalto3gvn/audio_params/tiny_hw.xml:system/etc/tiny_hw.xml \
+	device/samsung/vivalto3gvn/audio_params/codec_pga.xml:system/etc/codec_pga.xml \
+	device/samsung/vivalto3gvn/audio_params/audio_hw.xml:system/etc/audio_hw.xml \
+	device/samsung/vivalto3gvn/audio_params/audio_para:system/etc/audio_para \
+	device/samsung/vivalto3gvn/audio_params/audio_policy.conf:system/etc/audio_policy.conf \
 
 # Filesystem management tools
-PRODUCT_PACKAGES +=	\
-	setup_fs	\
-	e2fsck		\
-	f2fstat		\
-	fsck.f2fs	\
-	fibmap.f2fs	\
+PRODUCT_PACKAGES += \
+	setup_fs \
+	e2fsck \
+	f2fstat \
+	fsck.f2fs \
+	fibmap.f2fs \
 	mkfs.f2fs
+
+# Bluetooth config
+PRODUCT_COPY_FILES += \
+	device/samsung/vivalto3gvn/configs/bluetooth/bt_did.conf:system/etc/bluetooth/bt_did.conf \
+	device/samsung/vivalto3gvn/configs/bluetooth/bt_vendor.conf:system/etc/bluetooth/bt_vendor.conf
+
+# Bluetooth
+PRODUCT_PACKAGES += \
+	libbluetooth_jni
+
+# sprd telephony
+PRODUCT_PACKAGES += \
+	Dialer \
+	Mms
+
+# HWC
+PRODUCT_PACKAGES += \
+	libion \
+	iontest
+
+# libstagefrighthw
+PRODCUT_PACKAGES += \
+	libstagefrighthw
+
+# Lights
+PRODUCT_PACKAGES += \
+	lights.scx15
+
+# Device-specific packages
+PRODUCT_PACKAGES += \
+	Torch \
+	SamsungServiceMode
+
+# Bluetooth
+PRODUCT_PACKAGES += \
+	bluetooth.default \
+	audio.a2dp.default \
+	bt_vendor.conf
+
+# Audio
+PRODUCT_PACKAGES += \
+	audio_policy.scx15 \
+	audio.r_submix.default \
+	audio.usb.default
+
+# Wifi
+PRODUCT_COPY_FILES += \
+	device/samsung/vivalto3gvn/configs/wifi/wpa_supplicant_overlay.conf:system/etc/wifi/wpa_supplicant_overlay.conf
+
+# General config
+PRODUCT_COPY_FILES += \
+	device/samsung/vivalto3gvn/media/media_codecs.xml:system/etc/media_codecs.xml \
+	device/samsung/vivalto3gvn/media/media_profiles.xml:system/etc/media_profiles.xml \
+	device/samsung/vivalto3gvn/permissions/platform.xml:system/etc/permissions/platform.xml \
+	frameworks/native/data/etc/handheld_core_hardware.xml:system/etc/permissions/handheld_core_hardware.xml \
+	frameworks/native/data/etc/android.hardware.bluetooth.xml:system/etc/permissions/android.hardware.bluetooth.xml \
+	frameworks/native/data/etc/android.hardware.camera.autofocus.xml:system/etc/permissions/android.hardware.camera.autofocus.xml \
+	frameworks/native/data/etc/android.hardware.camera.flash-autofocus.xml:system/etc/permissions/android.hardware.camera.flash-autofocus.xml \
+	frameworks/native/data/etc/android.hardware.camera.front.xml:system/etc/permissions/android.hardware.camera.front.xml \
+	frameworks/native/data/etc/android.hardware.location.gps.xml:system/etc/permissions/android.hardware.location.gps.xml \
+	frameworks/native/data/etc/android.hardware.touchscreen.multitouch.xml:system/etc/permissions/android.hardware.touchscreen.multitouch.xml \
+	frameworks/native/data/etc/android.hardware.touchscreen.xml:system/etc/permissions/android.hardware.touchscreen.xml \
+	frameworks/native/data/etc/android.hardware.telephony.gsm.xml:system/etc/permissions/android.hardware.telephony.gsm.xml \
+	frameworks/native/data/etc/android.hardware.usb.accessory.xml:system/etc/permissions/android.hardware.usb.accessory.xml \
+	frameworks/native/data/etc/android.hardware.wifi.xml:system/etc/permissions/android.hardware.wifi.xml
+
+# Set default USB interface
+PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
+	persist.sys.usb.config=mtp
+
+# Device props
+PRODUCT_PROPERTY_OVERRIDES := \
+	keyguard.no_require_sim=true \
+	ro.com.android.dataroaming=false \
+	ro.msms.phone_count=2 \
+	persist.msms.phone_count=2 \
+	persist.msmslt=0 \
+	ro.modem.w.count=2 \
+	persist.sys.modem.diag=,gser \
+	sys.usb.gser.count=2 \
+	ro.ril.hsxpa=1 \
+	ro.ril.gprsclass=10 \
+	persist.radio.multisim.config=dsds \
+	ro.telephony.ril_class=SamsungBCMRIL
 
 # Support for Browser's saved page feature. This allows
 # for pages saved on previous versions of the OS to be
 # viewed on the current OS.
 PRODUCT_PACKAGES += \
-	libskia_legacy
+    libskia_legacy
 
 $(call inherit-product, hardware/broadcom/wlan/bcmdhd/config/config-bcm.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
