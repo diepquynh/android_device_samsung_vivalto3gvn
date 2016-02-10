16e51aa5d3cdf7f9c2cb829307ed9ea43aa7c1d4
Ngô Minh Quang <ngoquang2708@gmail.com>
2016-02-10 10:39:04 +0700

    Re-enable camera blob
    Re-enable camera blobs as camera source so not work for now

--- libs/hwcomposer/Android.mk
+++ libs/hwcomposer/Android.mk
@@ -37,8 +37,8 @@ LOCAL_SRC_FILES := SprdHWComposer.cpp \
 		   SprdExternalDisplayDevice/SprdExternalDisplayDevice.cpp \
 		   SprdUtil.cpp \
                    dump.cpp
-LOCAL_C_INCLUDES += $(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ/usr/include
-LOCAL_C_INCLUDES := \
+LOCAL_ADDITIONAL_DEPENDENCIES := $(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ/usr
+LOCAL_C_INCLUDES += \
 	$(LOCAL_PATH)/../gralloc \
 	$(LOCAL_PATH)/../mali/src/ump/include \
 	$(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ/usr/include/video/ \
