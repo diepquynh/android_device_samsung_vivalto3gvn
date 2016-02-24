13ae1d9060243d36c7ef50195a4e30306d626efe
Ngô Minh Quang <ngoquang2708@gmail.com>
2016-02-24 22:28:40 +0700

    libatchannel: new implementation, more stable
    Change-Id: I26bbcd75df060fb2ac98ef9e39a211b51d4b3260

--- libs/libatchannel/AtChannel.cpp
+++ libs/libatchannel/AtChannel.cpp
@@ -1,25 +1,30 @@
-#define LOG_TAG "IAtChannel"
+#define LOG_TAG "AtChannel"
+#define LOG_NDEBUG 0
 
-#include <cstdlib>
-#include <cstring>
 #include <android/log.h>
 #include <binder/IServiceManager.h>
 #include <utils/String8.h>
 #include <utils/String16.h>
-#include <dlfcn.h>
-#include <AtChannel.h>
 #include <cutils/properties.h>
 #include <cutils/sockets.h>
 #include <secril-client.h>
 #include <telephony/ril.h>
 #include <pthread.h>
+#include <time.h>
+#include <sys/time.h>
+#include <unistd.h>
+#include <errno.h>
+#include <stdlib.h>
+#include <string.h>
 
-using namespace android;
+#include <AtChannel.h>
 
-#define REQ_POOL_SIZE 32
 #ifndef MIN
 #define MIN(a, b) ((a) < (b) ? (a) : (b))
 #endif
+
+using namespace std;
+using namespace android;
 
 typedef enum _req_state {
 	REQ_STATE_PENDING,
@@ -27,157 +32,173 @@ typedef enum _req_state {
 	REQ_STATE_COMPLETED
 } req_state_t;
 
-typedef struct _req_status {
-	HRilClient client;
-	req_state_t state;
-	void *data;
-	size_t data_len;
-} req_status_t;
-
-req_status_t *req_status_pool[REQ_POOL_SIZE];
-pthread_mutex_t alloc_lock;
+static size_t num_rild = 1;
+static HRilClient ril_client[2];
+static pthread_mutex_t mutex[2];
+static pthread_cond_t cond[2];
+static req_state_t req_state[2];
+static char data[2][1024];
+static size_t datalen[2];
 
 static void init() __attribute__((constructor));
+static int cond_timedwait(pthread_mutex_t *mutex, pthread_cond_t *cond, unsigned long sec);
+static size_t create_at_req(void *out, size_t size, const char *at);
+static void print_hex(const void *data, size_t len);
+static int ril_on_unsolicited_handler(HRilClient handle, const void *data, size_t datalen);
+static int ril_on_complete_handler(HRilClient handle, const void *data, size_t datalen);
+static int ril_get_simId(HRilClient hrc);
+
+size_t sendAt(void *buf, size_t bufLen, int simId, const char* atCmd)
+{
+	size_t result = 0;
+	ALOGD("sendAt: [atCmd=%s]", atCmd);
+	ALOGD("sendAt: [simId=%d]", simId);
+	if (simId == 0 || simId == 1) {
+		char at[1024];
+		size_t at_len = create_at_req(at, sizeof(at), atCmd);
+		bool completed = false;
+		int ret;
+		pthread_mutex_lock(&mutex[simId]);
+		do {
+			ALOGI("InvokeOemRequestHookRaw: BEGIN");
+			ret = InvokeOemRequestHookRaw(ril_client[simId], at, at_len);
+			ALOGI("sendAt(): InvokeOemRequestHookRaw: END [%d]\n", ret);
+		} while (ret == RIL_CLIENT_ERR_AGAIN);
+		do {
+			ALOGI("cond_timedwait(): BEGIN");
+			do {
+				if (cond_timedwait(&mutex[simId], &cond[simId], 10UL) == ETIMEDOUT)
+					break;
+			} while (req_state[simId] == REQ_STATE_PENDING);
+			ALOGI("cond_timedwait(): END: [%d]\n", req_state[simId]);
+			switch (req_state[simId]) {
+			case REQ_STATE_UNSOL:
+				ALOGI("sendAt(): REQ_STATE_UNSOL: [");
+				print_hex(data[simId], datalen[simId]);
+				ALOGI("sendAt(): REQ_STATE_UNSOL: ]");
+				break;
+			case REQ_STATE_COMPLETED:
+				ALOGI("sendAt(): REQ_STATE_COMPLETED: [");
+				print_hex(data[simId], datalen[simId]);
+				ALOGI("sendAt(): REQ_STATE_COMPLETED: ]");
+				memset(buf, 0, bufLen);
+				memcpy(buf, data[simId], MIN(bufLen, datalen[simId]));
+				completed = true;
+				break;
+			case REQ_STATE_PENDING:
+				/* Timedout */
+				ALOGW("sendAt(): ######## TIMEDOUT! ########");
+				completed = true;
+				break;
+			}
+		} while (!completed);
+		if (req_state[simId] == REQ_STATE_COMPLETED)
+			result = datalen[simId];
+		pthread_mutex_unlock(&mutex[simId]);
+	}
+	return 0;
+}
+
+size_t create_at_req(void *out, size_t size, const char *at)
+{
+	char *ptr = (char *)out;
+	ptr[0] = 0x30;
+	ptr[1] = 0x00;
+	return sprintf(ptr + 2, "%s", at) + 2 + 1;
+}
+
+void print_hex(const void *data, size_t len)
+{
+	char hex_buf[1024] = { 0 };
+	if (len < 2*sizeof(hex_buf)) {
+		char *data_ptr = (char *) data;
+		char *ptr = hex_buf;
+		memset(hex_buf, 0, sizeof(hex_buf));
+		for (size_t i = 0; i < len; ++i) {
+			sprintf(ptr, "%02x", data_ptr[i]);
+			ptr += 2;
+		}
+		ALOGI("%s", hex_buf);
+	} else {
+		ALOGI("(null)");
+	}
+}
+
+int cond_timedwait(pthread_mutex_t *mutex, pthread_cond_t *cond, unsigned long sec)
+{
+	struct timespec time_to_wait;
+	struct timeval now;
+	gettimeofday(&now, NULL);
+	time_to_wait.tv_sec = now.tv_sec + sec;
+	time_to_wait.tv_nsec = now.tv_usec * 1000UL;
+	return pthread_cond_timedwait(cond, mutex, &time_to_wait);
+}
+
+int ril_on_unsolicited_handler(HRilClient handle, const void *data, size_t datalen)
+{
+	int simId;
+	if ((simId = ril_get_simId(handle)) >= 0) {
+		pthread_mutex_lock(&mutex[simId]);
+
+		memcpy(::data[simId], data, datalen);
+		::datalen[simId] = datalen;
+		req_state[simId] = REQ_STATE_UNSOL;
+
+		pthread_cond_signal(&cond[simId]);
+		pthread_mutex_unlock(&mutex[simId]);
+	}
+	return 0;
+}
+
+int ril_on_complete_handler(HRilClient handle, const void *data, size_t datalen)
+{
+	int simId;
+	if ((simId = ril_get_simId(handle)) >= 0) {
+		pthread_mutex_lock(&mutex[simId]);
+
+		memcpy(::data[simId], data, datalen);
+		::datalen[simId] = datalen;
+		req_state[simId] = REQ_STATE_COMPLETED;
+
+		pthread_cond_signal(&cond[simId]);
+		pthread_mutex_unlock(&mutex[simId]);
+	}
+	return 0;
+}
+
+int ril_get_simId(HRilClient hrc)
+{
+	if (hrc == ril_client[0])
+		return 0;
+	if (hrc == ril_client[1])
+		return 1;
+	return -1;
+}
 
 void init()
 {
-	pthread_mutex_init(&alloc_lock, NULL);
-}
-
-/*
- * Find free slot
- * Return NULL if there is no free slot
- */
-static req_status_t *req_status_alloc(int sim_id)
-{
-	int (*connect_ril)(HRilClient) = sim_id == 0 ? Connect_RILD : Connect_RILD_Second;
-	req_status_t *result = NULL;
-	pthread_mutex_lock(&alloc_lock);
-	for (int i = 0; i < REQ_POOL_SIZE; ++i) {
-		if (req_status_pool[i] == NULL &&
-				(result = (req_status_t *) malloc(sizeof(req_status_t)))) {
-			HRilClient client;
-			if ((client = OpenClient_RILD()) && (*connect_ril)(client) == 0) {
-				result->client = client;
-				result->state = REQ_STATE_PENDING;
-				result->data = NULL;
-				result->data_len = 0;
-				req_status_pool[i] = result;
-			} else {
-				CloseClient_RILD(client);
-				free(result);
-				result = NULL;
-			}
-			break;
-		}
+	char prop[PROPERTY_VALUE_MAX];
+	property_get("ro.modem.w.count", prop, "1");
+	ALOGD("num_rild: [%d]", (num_rild = atoi(prop)));
+	if (num_rild > 2) {
+		ALOGW("num_rild: [%d]", num_rild);
+		ALOGW("num_rild: set to 2");
+		num_rild = 2;
 	}
-	pthread_mutex_unlock(&alloc_lock);
-	return result;
-}
-
-static int req_status_free(req_status_t *req_status)
-{
-	pthread_mutex_lock(&alloc_lock);
-	for (int i = 0; i < REQ_POOL_SIZE; ++i) {
-		if (req_status_pool[i] == req_status) {
-			req_status_pool[i] = NULL;
-			break;
-		}
-	}
-	pthread_mutex_unlock(&alloc_lock);
-	free(req_status->data);
-	CloseClient_RILD(req_status->client);
-	free(req_status);
-	return 0;
-}
-
-static req_status_t *req_status_find_for(HRilClient client)
-{
-	for (int i = 0; i < REQ_POOL_SIZE; ++i) {
-		req_status_t *result = req_status_pool[i];
-		if (result && result->client == client)
-			return result;
-	}
-	return NULL;
-}
-
-static size_t make_raw_at_req(void *out, int outSize, const char *atCmd, const int atCmdLen)
-{
-	const char atMagic[] = { 0x30, 0x00 };
-	const int atReqSize = sizeof(atMagic) + atCmdLen + 1;
-	if (outSize >= atReqSize) {
-		char *outPtr = (char *)out;
-		memcpy(outPtr, atMagic, sizeof(atMagic));
-		outPtr += sizeof(atMagic);
-		memcpy(outPtr, atCmd, atCmdLen + 1);
-		return atReqSize;
-	}
-	return 0;
-}
-
-static int ril_on_unsolicited_handler(HRilClient handle, const void *data, size_t datalen)
-{
-	req_status_t *req_status = req_status_find_for(handle);
-	if (req_status) {
-		req_status->data = malloc(datalen);
-		memcpy(req_status->data, data, datalen);
-		req_status->data_len = datalen;
-		req_status->state = REQ_STATE_UNSOL;
-	}
-	return 0;
-}
-
-static int ril_on_complete_handler(HRilClient handle, const void *data, size_t datalen)
-{
-	req_status_t *req_status = req_status_find_for(handle);
-	if (req_status) {
-		req_status->data = malloc(datalen);
-		memcpy(req_status->data, data, datalen);
-		req_status->data_len = datalen;
-		req_status->state = REQ_STATE_COMPLETED;
-	}
-	return 0;
-}
-
-size_t sendAt(void *out, size_t outLen, int simId, const char* atCmd)
-{
-	req_status_t *req_status = req_status_alloc(simId);
-	if (req_status) {
-		char raw_at[1024];
-		int raw_at_size = make_raw_at_req(raw_at, sizeof(raw_at), atCmd, strlen(atCmd));
-		bool completed = false;
+	for (int i = 0; i < num_rild; ++i) {
+		ALOGD("pthread_mutex_init: [%d] [%d]", i, pthread_mutex_init(&mutex[i], NULL));
+		ALOGD("pthread_cond_init: [%d] [%d]", i, pthread_cond_init(&cond[i], NULL));
+		ALOGD("OpenClient_RILD: [%d] [%p]", i, (ril_client[i] = OpenClient_RILD()));
+		ALOGD("Connect_RILD: [%d] [%d]", i, Connect_RILD(ril_client[i]));
 		ALOGI("RegisterUnsolicitedHandler: %d\n",
 				RegisterUnsolicitedHandler(
-						req_status->client,
+						ril_client[i],
 						RIL_REQUEST_OEM_HOOK_RAW,
 						ril_on_unsolicited_handler));
 		ALOGI("RegisterRequestCompleteHandler: %d\n",
 				RegisterRequestCompleteHandler(
-						req_status->client,
+						ril_client[i],
 						RIL_REQUEST_OEM_HOOK_RAW,
 						ril_on_complete_handler));
-		ALOGI("InvokeOemRequestHookRaw: %d\n",
-				InvokeOemRequestHookRaw(req_status->client, raw_at, raw_at_size));
-		do {
-			switch (req_status->state) {
-			case REQ_STATE_UNSOL:
-				;
-				break;
-			case REQ_STATE_COMPLETED:
-				memset(out, 0, outLen);
-				memcpy(out, req_status->data, MIN(outLen, req_status->data_len));
-				completed = true;
-				break;
-			case REQ_STATE_PENDING:
-			default:
-				sleep(1); // TODO Use locking
-				ALOGI("Sleeping...");
-				break;
-			}
-		} while (!completed);
-		size_t result = req_status->data_len;
-		req_status_free(req_status);
-		return result;
 	}
-	return -1;
 }
