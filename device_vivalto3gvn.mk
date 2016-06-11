c7ca1638c9e0d8ddd9b4ed9671542cc253b4eece
Ngô Minh Quang <ngoquang2708@gmail.com>
2016-06-11 19:19:38 +0700

    Remove duplicated include of full_base_telephony.mk
    Change-Id: I099bc158abb88dff04518fd49c32288d5e4176de

--- device_vivalto3gvn.mk
+++ device_vivalto3gvn.mk
@@ -200,8 +200,6 @@ PRODUCT_PROPERTY_OVERRIDES += \
 PRODUCT_PACKAGES += \
 	libskia_legacy
 
-$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
-
 # Set those variables here to overwrite the inherited values.
 PRODUCT_NAME := full_vivalto3gvn
 PRODUCT_DEVICE := vivalto3gvn
