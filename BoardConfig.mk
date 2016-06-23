fcc58bece76cd673dd0ab05d74aaefbaae203e68
Ngô Minh Quang <ngoquang2708@gmail.com>
2016-06-23 19:43:15 +0700

    healthd: opt-out of enabling charger LED since we do not have it
    Change-Id: Ibd13cb61ec58c9ead988bcb765512100c050875e

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -101,6 +101,7 @@ BOARD_HAL_STATIC_LIBRARIES := libhealthd.scx15
 
 # Charger
 BOARD_CHARGER_ENABLE_SUSPEND := true
+BOARD_NO_CHARGER_LED := true
 BOARD_CHARGING_MODE_BOOTING_LPM := /sys/class/power_supply/battery/batt_lp_charging
 CHARGING_ENABLED_PATH := /sys/class/power_supply/battery/batt_lp_charging
 BACKLIGHT_PATH := /sys/class/backlight/panel/brightness
