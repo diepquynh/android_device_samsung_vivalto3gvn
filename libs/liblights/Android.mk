63b81b826f5ba7d59fd9cdfb7aeefe5e5a6d6133
Ngô Minh Quang <ngoquang2708@gmail.com>
2016-07-02 21:13:13 +0700

    hal: light: cleanup
    Change-Id: I4e94c425687693a1f848980c1e7ced87c362cab0

--- libs/liblights/Android.mk
+++ libs/liblights/Android.mk
@@ -1,4 +1,7 @@
+#
 # Copyright (C) 2008 The Android Open Source Project
+#
+# Copyright (C) 2016 The CyanogenMod Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -11,11 +14,9 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+#
 
 LOCAL_PATH:= $(call my-dir)
-
-# HAL module implemenation stored in
-# hw/<COPYPIX_HARDWARE_MODULE_ID>.<ro.board.platform>.so
 
 include $(CLEAR_VARS)
 LOCAL_SRC_FILES := lights.c
