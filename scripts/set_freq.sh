283efbe62da9651deda598cacd17f2ce2fa3e738
Ngô Minh Quang <ngoquang2708@gmail.com>
2016-07-31 19:14:42 +0700

    Reduce set_freq service sleep time
    Change-Id: Ifec6cbdb1f7b5011f51d460fa987cda6d07d7c95

--- scripts/set_freq.sh
+++ scripts/set_freq.sh
@@ -2,6 +2,6 @@
 
 devNode=/sys/devices/platform/scxx30-dmcfreq.0/devfreq/scxx30-dmcfreq.0/ondemand/set_freq
 
-while [ ! -e $devNode ]; do sleep 10; done
+while [ ! -e $devNode ]; do sleep 3; done
 chown media.system $devNode
 chmod 660 $devNode
