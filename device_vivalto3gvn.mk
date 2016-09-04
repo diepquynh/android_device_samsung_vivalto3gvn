96e3dcc4ec0ddaf81c15764b8bd3f63298610c63
Quang Ngô <ngoquang2708@gmail.com>
2016-09-04 11:40:59 +0700

    init: override system cameraserver.rc to use LD_SHIM_LIBS

--- device_vivalto3gvn.mk
+++ device_vivalto3gvn.mk
@@ -66,6 +66,7 @@ PRODUCT_COPY_FILES += \
 # System init .rc files
 SYSTEM_INIT_RC_FILES := \
 	device/samsung/vivalto3gvn/system/etc/init/at_distributor.rc \
+	device/samsung/vivalto3gvn/system/etc/init/cameraserver.rc \
 	device/samsung/vivalto3gvn/system/etc/init/chown_service.rc \
 	device/samsung/vivalto3gvn/system/etc/init/data.rc \
 	device/samsung/vivalto3gvn/system/etc/init/engpc.rc \
