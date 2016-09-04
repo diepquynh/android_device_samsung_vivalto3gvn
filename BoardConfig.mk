af46d4e4539b5ff90ad60388ea37bf9a154f51fc
Quang Ngô <ngoquang2708@gmail.com>
2016-09-04 09:05:58 +0700

    config: remove obsoluted EXPERIMENTAL_USE_JAVA8 flag
    This flag is no longer needed in N. In fact, JDK 8 is required to be able
    to build Android N.

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -135,9 +135,6 @@ TARGET_RECOVERY_PIXEL_FORMAT := "RGBX_8888"
 TARGET_RECOVERY_FSTAB := device/samsung/vivalto3gvn/rootdir/fstab.scx15
 TARGET_RECOVERY_TWRP := false
 
-# Build system
-EXPERIMENTAL_USE_JAVA8 := true
-
 ifeq ($(TARGET_RECOVERY_TWRP),true)
 RECOVERY_VARIANT := twrp
 TARGET_USES_LOGD := true
