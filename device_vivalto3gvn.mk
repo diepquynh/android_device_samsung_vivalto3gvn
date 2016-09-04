fbdbdc07ec979c763c024187d54d4529e57ba3b0
Quang Ngô <ngoquang2708@gmail.com>
2016-09-04 11:39:17 +0700

    init: move engpc services to engpc.rc

--- device_vivalto3gvn.mk
+++ device_vivalto3gvn.mk
@@ -68,6 +68,7 @@ SYSTEM_INIT_RC_FILES := \
 	device/samsung/vivalto3gvn/system/etc/init/at_distributor.rc \
 	device/samsung/vivalto3gvn/system/etc/init/chown_service.rc \
 	device/samsung/vivalto3gvn/system/etc/init/data.rc \
+	device/samsung/vivalto3gvn/system/etc/init/engpc.rc \
 	device/samsung/vivalto3gvn/system/etc/init/gpsd.rc \
 	device/samsung/vivalto3gvn/system/etc/init/hostapd.rc \
 	device/samsung/vivalto3gvn/system/etc/init/kill_phone.rc \
